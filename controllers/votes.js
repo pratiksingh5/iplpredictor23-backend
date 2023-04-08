a4524f9baa06d9ccfa5b5cabdc277b38992e99e9
Pratik Singh <pratikrajaryan@gmail.com>
2023-04-08 15:10:39 +0530

    user match api added

--- controllers/votes.js
+++ controllers/votes.js
@@ -1,5 +1,6 @@
 import Vote from "../models/Vote.js";
 import User from "../models/User.js";
+import Match from "../models/Match.js";
 
 /* VOTE BY USER */
 
@@ -8,11 +9,20 @@ export const makeVote = async (req, res) => {
     const { userId, matchId, selectedTeam } = req.body;
     // create a new Vote object to represent the user's vote
     const user = await User.findById(userId);
+    const existingVote = user.votes.find((vote) => vote.matchId === matchId);
+    if (existingVote) {
+      res.status(400).send("You have already voted for this match");
+    } else {
+      user.votes.push({
+        matchId,
+        selectedTeam,
+      });
+    }
     // update the user's record with the new vote
-    user.votes.push({
-      matchId,
-      selectedTeam,
-    });
+    // user.votes.push({
+    //   matchId,
+    //   selectedTeam,
+    // });
     // save the updated user record to the database
     await user.save();
     const vote = new Vote({
@@ -33,3 +43,33 @@ export const makeVote = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+
+export const getUserMatches = async(req, res) => {
+  try {
+    const user = await User.findById(req.params.userId).populate('votes.match');
+    const votes = user.votes;
+    const matches = await Match.find();
+    const matchData = [];
+
+    votes.forEach(vote => {
+      const match = matches.find(match => match._id && match._id.toString() == vote.matchId);
+      if (match) {
+        matchData.push({
+          matchId: vote.matchId,
+          matchNo: match.matchNo,
+          team1: match.team1,
+          team2: match.team2,
+          selectedTeam: vote.selectedTeam,
+          isWinner: vote.isCorrected
+        });
+      }
+    });
+
+    res.status(200).json(matchData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
--- routes/votes.js
+++ routes/votes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { makeVote } from "../controllers/votes.js";
+import { makeVote, getUserMatches } from "../controllers/votes.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post('/makevote', verifyToken, makeVote);
+router.get('/:userId', getUserMatches)
 
 export default router;
