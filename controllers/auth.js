f5676012a5fe4e898a3eaaaf4525b75d840c03b8
Pratik Singh <pratikrajaryan@gmail.com>
2024-03-23 11:51:40 +0530

    remove upload functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,48 +4,11 @@ import User from "../models/User.js";
 import cloudinary from "cloudinary";
 import nodemailer from "nodemailer";
 
-// Configuration 
-cloudinary.config({
-  cloud_name: "vbushoutout",
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true  
-});
-
-/* REGISTER USER */
 export const register = async (req, res) => {
   try {
-    const {
-      name,
-      instaUsername,
-      email,
-      password,
-      picturePath,
-    } = req.body;
-
-    cloudinary.config({
-      cloud_name: "vbushoutout",
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-      secure: true  
-    });
-
+    const { email, password, name, instaUsername } = req.body;
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
-
-    // let tempraryImageDirectory = '/tmp/';
-    // const usersPath = path.join(process.cwd(), tempraryImageDirectory, picturePath);
-    // console.log(usersPath)
-
-    let myUpload;
-    try {
-      myUpload = await cloudinary.uploader.upload('public/assets/' + picturePath);
-      // myUpload = await cloudinary.uploader.upload(usersPath);
-    } catch (uploadErr) {
-
-      throw new Error('Error uploading picture: ' + uploadErr);
-    }
-
 
     const newUser = new User({
       name,
@@ -53,8 +16,8 @@ export const register = async (req, res) => {
       email,
       password: passwordHash,
       picturePath: {
-        public_id: myUpload.public_id,
-        url: myUpload.secure_url
+        public_id: "default_img",
+        url: "https://res.cloudinary.com/vbushoutout/image/upload/v1711174540/THBM-LOGO2_yt6nlf.jpg",
       },
     });
     const savedUser = await newUser.save();
@@ -64,6 +27,58 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+/* REGISTER USER */
+// export const register = async (req, res) => {
+//   try {
+//     const {
+//       name,
+//       instaUsername,
+//       email,
+//       password,
+//       // picturePath,
+//     } = req.body;
+
+//     // cloudinary.config({
+//     //   cloud_name: "vbushoutout",
+//     //   api_key: process.env.CLOUDINARY_API_KEY,
+//     //   api_secret: process.env.CLOUDINARY_API_SECRET,
+//     //   secure: true
+//     // });
+
+//     const salt = await bcrypt.genSalt();
+//     const passwordHash = await bcrypt.hash(password, salt);
+
+//     // let tempraryImageDirectory = '/tmp/';
+//     // const usersPath = path.join(process.cwd(), tempraryImageDirectory, picturePath);
+//     // console.log(usersPath)
+
+//     // let myUpload;
+//     // try {
+//     //   myUpload = await cloudinary.uploader.upload('public/assets/' + picturePath);
+//     //   // myUpload = await cloudinary.uploader.upload(usersPath);
+//     // } catch (uploadErr) {
+
+//     //   throw new Error('Error uploading picture: ' + uploadErr);
+//     // }
+
+//     const newUser = new User({
+//       name,
+//       instaUsername,
+//       email,
+//       password: passwordHash,
+//       picturePath: {
+//         public_id: "test",
+//         url: "test_url"
+//       },
+//     });
+//     const savedUser = await newUser.save();
+//     res.status(201).json(savedUser);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ error: err.message });
+//   }
+// };
 
 /* LOGGING IN */
 export const login = async (req, res) => {
@@ -83,63 +98,61 @@ export const login = async (req, res) => {
   }
 };
 
-
-export const forgotPassword = async(req, res) => {
+export const forgotPassword = async (req, res) => {
   try {
     const { email } = req.body;
     const user = await User.findOne({ email });
-    
+
     if (!user) {
-      return res.status(404).send('User not found');
+      return res.status(404).send("User not found");
     }
 
     // Generate OTP
     const OTP = Math.floor(100000 + Math.random() * 900000);
-    
+
     // Update user document with OTP
     user.OTP = OTP;
     user.OTPExpiration = Date.now() + 600000; // OTP valid for 10 minutes
     await user.save();
-    
+
     // Send OTP to user email
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: "Gmail",
       secure: true,
       auth: {
         // user: 'vbushoutout@gmail.com',
-        user: 'thedialoguesaga@gmail.com',
-        pass: process.env.VBU_PASSWORD
-      }
+        user: "thedialoguesaga@gmail.com",
+        pass: process.env.VBU_PASSWORD,
+      },
     });
 
     const mailOptions = {
-      from: 'thehumorbro',
+      from: "thehumorbro",
       to: email,
-      subject: 'Reset your password for your IPL Predictor 2023',
-      text: `Use the following OTP to reset your password: ${OTP}`
+      subject: "Reset your password for your IPL Predictor 2023",
+      text: `Use the following OTP to reset your password: ${OTP}`,
     };
-    
+
     await transporter.sendMail(mailOptions);
-    
-    res.status(200).send({ message: 'OTP sent to email' });
+
+    res.status(200).send({ message: "OTP sent to email" });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
-}
-
+};
 
 export const resetPassword = async (req, res) => {
   try {
     const { email, OTP, newPassword } = req.body;
     const user = await User.findOne({ email, OTP });
-    
+
     if (!user) {
-      return res.status(400).send({ message: 'Invalid OTP' });
+      return res.status(400).send({ message: "Invalid OTP" });
     }
-    
+
     if (user.OTPExpiration < Date.now()) {
-      return res.status(400).send({ message: 'OTP Expired' } );
+      return res.status(400).send({ message: "OTP Expired" });
     }
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(newPassword, salt);
@@ -148,10 +161,10 @@ export const resetPassword = async (req, res) => {
     user.OTP = null;
     user.OTPExpiration = null;
     await user.save();
-    
-    res.status(200).send({ message: 'Password updated successfully' } );
+
+    res.status(200).send({ message: "Password updated successfully" });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
-}
+};
--- index.js
+++ index.js
@@ -48,10 +48,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
-// app.post("/posts", verifyToken, upload.single("picture"), createPost);
-
-
+// app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/register", register);
 app.get('/getAllUsers', async(req,res)=> {
     try {
       const users = await User.find();
